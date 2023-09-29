08262c952a4fdd38dee53c126c9374fca0d40036
Tait Ralston <taitralston@gmail.com>
2023-09-28 21:29:51 -0700

    Code clean up

--- leaflet-part-1/static/js/logic.js
+++ leaflet-part-1/static/js/logic.js
@@ -9,30 +9,25 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
   
-// Load the GeoJSON data
+// Load the GeoJSON earthquake data
 let eqData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson";
   
 // Define function to determine circle marker size based on earthquake magnitude
 function circleSize(magnitude) {
-
+    
+    // Define max and min circle marker size for earthquake mag display
     const minRadius = 1000;
     const maxRadius = 80000;
 
+    // Display the circle marker by increasing magnitude size 
     return minRadius + (magnitude * (maxRadius - minRadius));
-
-    // Adjust the scaling factor to correpond with earthquake magnitude
-    // const circleScale = 5;
-    // const maxSize = 50;
-    // Circle size based on earthquake magnitude
-    // const circleSize = magnitude * circleScale;
-    // Ensure the circle size does not exceed the maximum size
-    // return Math.max(circleSize, maxSize);
     
 };
 
-// Define a function to determine circle color based on earthquake depth
+// Define function to determine circle color based on earthquake depth
 function circleColor(depth) {
-    // You can customize the colors and depth ranges as needed
+
+    // Customize the colors and depth ranges
     if (depth >= -10 && depth <= 10) {
         return "#0ef022";
     } else if (depth > 10 && depth <= 30) {
@@ -48,14 +43,14 @@ function circleColor(depth) {
     }
 };
 
-// Get the data with d3
+// Get the earthquake data from the geojson api with d3
 d3.json(eqData).then(function(data) {
     console.log(data);
 
-    // Create a new marker cluster group
+    // Create a new layer for the earthquake data
     let eqLayer = L.featureGroup();
 
-    // Loop through the data
+    // Loop through the earthquake data
     data.features.forEach(function(features) {
 
         // Set the data points to a variable for the popup information
@@ -76,10 +71,10 @@ d3.json(eqData).then(function(data) {
         eqLayer.addLayer(circleMarker);
     });
 
-    // Add the earthquake circles to the map
+    // Add the earthquake marker circles to the map
     myMap.addLayer(eqLayer);
 
-    // Create a legend
+    // Create a legend for earthquake depth ranges
     let eqLegend = L.control({position: 'bottomright'});
 
     // Include earthquake depth ranges and colors in the legend
@@ -98,6 +93,6 @@ d3.json(eqData).then(function(data) {
         return div;
     };
 
-    // Add the legend to the map
+    // Add the earthquake depth legend to the map
     eqLegend.addTo(myMap);
 });
